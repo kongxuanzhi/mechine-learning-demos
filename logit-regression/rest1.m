20166c9cfd410267db05f554cf1916b84071e9c2
kongxuanzhi <875248646@qq.com>
2019-02-25 20:15:27 +0800

    add 1 fold cross-validation and 10 fold cross-validation for 10 times

--- logit-regression/rest1.m
+++ logit-regression/rest1.m
@@ -1,4 +1,6 @@
 % 初始化参数和全局变量
+old_error = 0;
+Iter = 0; %迭代次数
 
 % 加载样本数据
 % 1-2组
@@ -16,43 +18,46 @@ y = y - 1;
 % y = y / 2;
 % x = [x0;x1];
 % y = [y0;y1];
-[m, n] = size(y);
 
+[m, n] = size(y);
 % 10次10折交叉验证
-err0 = 0;
-for k=0:9
-%     old_error = 0;
-    err0 = 0;
-    Iter = 1; %迭代次数
+for k=1:m
     % 循环迭代
+    err0 = 0;
     B = [0;0;0;0;1]; %参数，列向量为准
-    
-    old_error = zeros(100,1);
     while(1)
         bx = zeros(m, 1);
         cur_error = 0;
+        add_error = zeros(m, 1);
         % 循环样本数据计算误差
         for i=1:m
             %跳过用于验证的样本
-            if fix(mod(i-1, 50)/5) == k
+            if i == k
                 continue;
             end
 %           x(i,:)
+
             bx(i) = x(i,:) * B;
-            cur_error = cur_error + (-y(i) * bx(i) + log(1 + exp(bx(i))));
+            tlog = bx(i);
+            if abs(bx(i)) < 30
+                tlog = log(1 + exp(bx(i)));
+            end
+
+            add_error(i) = 1/(1 + exp(bx(i)));
+            cur_error = cur_error + (-y(i) * bx(i) + tlog);
         end
         % 比较两次误差，决定是否跳出迭代
-        if abs(old_error(Iter) - cur_error) < 0.000001
+        if abs(old_error - cur_error) < 0.0001
             break;
         end 
         % 更新参数（牛顿法或梯度下降法）
         Iter = Iter + 1;
-        old_error(Iter) = cur_error;
+        old_error = cur_error;
         db1 = 0;
         db2 = 0;
 
         for i = 1:m
-            if fix(mod(i-1, 50)/5) == k
+            if i == k
                 continue;
             end
             
@@ -60,24 +65,21 @@ for k=0:9
             db1 = db1 - x(i,:) * (y(i) - p1);
             db2 = db2 + x(i,:).' * x(i,:) * p1 * (1 - p1);
         end
-%         B = B - db1.';
         B = B - (pinv(db2) * db1.');
     % 迭代结束
-
     end
-
+    
     %用验证集验证准确度
-    for i=0:1
-        for j=1:5
-            y1 = -x(k*5 + 50*i + j,:) * B;
-            tmp = 1/(1+exp(y1));
-            tmp = (tmp>=0.5);
-            err0 = err0 + (tmp ~= y(i*50 + 5*k + j));
-        end
-    end
 
+    y1 = -x(k,:) * B;
+    tmp = 1/(1+exp(y1));
+    tmp = (tmp>=0.5);
+    err0 = err0 + (tmp ~= y(k));
+    disp(err0);
 end
 disp(err0);
+pause(1);
 
+    
 % 输出迭代结果，画出图像
 % output B
